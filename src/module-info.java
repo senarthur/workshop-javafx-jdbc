48458d02f21aaab17d59433f3808326f0890d369
Arthur Sena <sena.arthur698@gmail.com>
2024-09-12 10:29:57 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,7 @@ module workshop_java_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
-	//opens gui to javafx.fxml;	
+	opens gui to javafx.fxml;	
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
